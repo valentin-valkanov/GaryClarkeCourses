b3fc6ffd1996a3565bd722121a41fc4112cbb233
Valentin Valkanov <valentinvalkanof@gmail.com>
2023-12-26 13:50:00 +0200

    Adding Routing and Controlers changes before merging the brnach with main

---
+++ PHPFrameworkPRO/framework/Http/Kernel.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace GaryClarke\Framework\Http;
+
+class Kernel
+{
+    public function handle(Request $request): Response
+    {
+        $content = '<h1>Hello World</h1>';
+        return new Response($content);
+    }
+}
