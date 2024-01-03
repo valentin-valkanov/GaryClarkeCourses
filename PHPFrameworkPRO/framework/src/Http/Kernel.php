24e1767aaeb84e5f9f12e3b2b926be50ab2542da
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-01-03 13:52:55 +0200

    EnvoirmentModels

--- PHPFrameworkPRO/framework/src/Http/Kernel.php
+++ PHPFrameworkPRO/framework/src/Http/Kernel.php
@@ -8,27 +8,46 @@ use Psr\Container\ContainerInterface;
 
 class Kernel
 {
+    private string $appEnv;
+
     public function __construct(
         private RouterInterface $router,
         private ContainerInterface $container
     )
     {
+        $this->appEnv = $this->container->get('APP_ENV');
     }
 
     public function handle(Request $request): Response
     {
         try {
 
+            throw new \Exception('Kernel exception');
+
             [$routeHandler, $vars] = $this->router->dispatch($request, $this->container);
 
             $response = call_user_func_array($routeHandler, $vars);
 
-        } catch (HttpException $exception) {
-            $response = new Response($exception->getMessage(), $exception->getStatusCode());
         } catch (\Exception $exception) {
-            $response = new Response($exception->getMessage(), 500);
+            $response = $this->createExceptionResponse($exception);
         }
 
         return $response;
+    }
+
+    /**
+     * @throws  \Exception $exception
+     * @return Response
+     */
+    private function createExceptionResponse(\Exception $exception):Response
+    {
+        if(in_array($this->appEnv, ['dev', 'test'])){
+            throw $exception;
+        }
+        if($exception instanceof HttpException){
+            return new Response($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        return new Response('Server error', Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 }
