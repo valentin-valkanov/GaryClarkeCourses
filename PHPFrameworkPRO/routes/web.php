b3f62ad8acee972bfabd6dc10f453f15512856ac
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-01-05 12:55:12 +0200

    CreatinAnInputForm

--- PHPFrameworkPRO/routes/web.php
+++ PHPFrameworkPRO/routes/web.php
@@ -7,6 +7,7 @@ use GaryClarke\Framework\Http\Response;
 return [
     ['GET', '/', [HomeController::class, 'index']],     //[$method, $vars, $handler]
     ['GET', '/posts/{id:\d+}', [PostsController::class, 'show']],
+    ['GET', '/posts', [PostsController::class, 'create']],
     ['GET', '/hello/{name:.+}', function(string $name) {
         return new Response("Hello $name");
     }]
