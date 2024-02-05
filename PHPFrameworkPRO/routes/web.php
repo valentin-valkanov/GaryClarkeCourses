3cbc457a7647904cbbc4f9b8055a15f3f1094db6
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-02-05 23:17:12 +0200

    submittinf-form

--- PHPFrameworkPRO/routes/web.php
+++ PHPFrameworkPRO/routes/web.php
@@ -8,6 +8,7 @@ return [
     ['GET', '/', [HomeController::class, 'index']],     //[$method, $vars, $handler]
     ['GET', '/posts/{id:\d+}', [PostsController::class, 'show']],
     ['GET', '/posts', [PostsController::class, 'create']],
+    ['POST', '/posts', [PostsController::class, 'store']],
     ['GET', '/hello/{name:.+}', function(string $name) {
         return new Response("Hello $name");
     }]
