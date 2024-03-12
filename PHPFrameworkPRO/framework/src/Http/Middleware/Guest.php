abc506e5df57223304bcf793f9f04fe35a9b0233
Valentin Valkanov <valentinvalkanof@gmail.com>
2024-03-12 14:52:49 +0200

    guest-middleware

---
+++ PHPFrameworkPRO/framework/src/Http/Middleware/Guest.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace GaryClarke\Framework\Http\Middleware;
+
+use GaryClarke\Framework\Authentication\SessionAuthentication;
+use GaryClarke\Framework\Http\RedirectResponse;
+use GaryClarke\Framework\Http\Request;
+use GaryClarke\Framework\Http\Response;
+use GaryClarke\Framework\Session\SessionInterface;
+
+class Guest implements MiddleWareInterface
+{
+    public function __construct(private SessionInterface $session)
+    {
+    }
+
+    public function process(Request $request, RequestHandlerInterface $requestHandler): Response
+    {
+        $this->session->start();
+
+        if ($this->session->has(SessionAuthentication::AUTH_KEY)) {
+            return new RedirectResponse('/dashboard');
+        }
+
+        return $requestHandler->handle($request);
+    }
+}
--- PHPFrameworkPRO/routes/web.php
+++ PHPFrameworkPRO/routes/web.php
@@ -15,8 +15,18 @@ return [
     ['GET', '/posts', [PostsController::class, 'create']],
     ['POST', '/posts', [PostsController::class, 'store']],
     ['GET', '/register', [RegistrationController::class, 'index']],
-    ['POST', '/register', [RegistrationController::class, 'register']],
-    ['GET', '/login', [LoginController::class, 'index']],
+    ['POST', '/register', [RegistrationController::class, 'register',
+        [
+            \GaryClarke\Framework\Http\Middleware\Guest::class
+        ]
+    ]
+    ],
+    ['GET', '/login', [LoginController::class, 'index',
+        [
+            \GaryClarke\Framework\Http\Middleware\Guest::class
+        ]
+    ]
+    ],
     ['POST', '/login', [LoginController::class, 'login']],
     ['GET', '/dashboard', [DashboardController::class, 'index',
         [
